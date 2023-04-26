88fb195aabf134ae10430a2fe734c0170940d7f0
Amit <amit@techfra.in>
2023-04-26 10:29:23 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -1,39 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-import Men from "./Pages/Men";
-import Women from "./Pages/Women";
-import Kids from "./Pages/Kids";
-import Home from "./Pages/Home";
-import ViewProduct from "./Components/ViewProduct";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {Router} from './Router';
+import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route index element={<Home />} />
-      <Route path="men" element={<Men />} />
-      <Route path="women" element={<Women />} />
-      <Route path="kids" element={<Kids />} />
-      <Route path="view" element={<ViewProduct />} />
-    </Route>
-  )
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <Router />
 );
-// root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
